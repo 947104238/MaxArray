4c722cdaea09aa1fcb7e263b49ca4b54df274180
947104238 <wfl1997@qq.com>
2018-03-25 17:08:54 +0800

    11

--- app/src/test/java/com/aierdeliqi/junittestexample/MaxArrayTest.java
+++ app/src/test/java/com/aierdeliqi/junittestexample/MaxArrayTest.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by admin on 2018/3/24.
+ * Created by Quin on 2018/3/24.
+ * version 1.1
  */
 public class MaxArrayTest {
     @Test
     public void searchMaxArray() throws Exception {
-        assertEquals(20,new MaxArray().searchMaxArray());
+//        assertEquals(20,new MaxArray(0).searchMaxArray());
+//        assertEquals(6,new MaxArray(new int[]{-4,1,2,3}).searchMaxArray());
+//        assertEquals(10,new MaxArray(new int[]{1,2,3,4}).searchMaxArray());
+//        assertEquals(0,new MaxArray(new int[]{-1,-2,-3,-4}).searchMaxArray());
+        assertEquals(1,new MaxArray(new int[]{0,0,0,1}).searchMaxArray());
     }
 
 }
